bbebf13dd3733ec65d3b2e2fb4b386cf4f47195d
Mack Hendricks <mack@dopensource.com>
2019-11-01 10:08:46 -0400

    Updated

--- remote-backend/run.sh
+++ remote-backend/run.sh
@@ -1,4 +1,5 @@
-export TOKEN=$TOKEN
+export TERRAFORM_CONFIG=/Users/mack/Documents/tfe-api/remote-backend/.terraformrc
+export TOKEN=$PTFE_TOKEN
 terraform init -backend-config="token=$TOKEN" 
 terraform apply
 
